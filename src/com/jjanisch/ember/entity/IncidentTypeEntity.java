4e679a294ad67846829ecc6ae807df2058047a98
Justin Janisch <jjanisch@charter.net>
2016-03-28 14:49:37 -0500

    Added IncidentType Dao

--- src/com/jjanisch/ember/entity/IncidentTypeEntity.java
+++ src/com/jjanisch/ember/entity/IncidentTypeEntity.java
@@ -24,6 +24,14 @@ public class IncidentTypeEntity {
     }
 
     @Override
+    public String toString() {
+        return "IncidentTypeEntity{" +
+                "code=" + code +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
