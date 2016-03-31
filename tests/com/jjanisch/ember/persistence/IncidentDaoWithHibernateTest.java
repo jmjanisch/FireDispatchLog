8e5154627e826a64b5e1e9cff30d5e738ee98e6e
Justin Janisch <jjanisch@charter.net>
2016-03-31 10:38:55 -0500

    Build Incident Add Test

--- tests/com/jjanisch/ember/persistence/IncidentDaoWithHibernateTest.java
+++ tests/com/jjanisch/ember/persistence/IncidentDaoWithHibernateTest.java
@@ -1,6 +1,7 @@
 package com.jjanisch.ember.persistence;
 
 import com.jjanisch.ember.entity.IncidentEntity;
+import com.jjanisch.ember.entity.Staff;
 import com.jjanisch.ember.entity.StaffEntity;
 import com.jjanisch.ember.entity.ApparatusEntity;
 
@@ -20,23 +21,55 @@ public class IncidentDaoWithHibernateTest {
 
     @Test
     public void testAddIncident() throws Exception {
-        IncidentTypeDaoWithHibernate incidentDao = new IncidentTypeDaoWithHibernate();
+        IncidentDaoWithHibernate incidentDao = new IncidentDaoWithHibernate();
         ApparatusDaoWithHibernate apparatusDao = new ApparatusDaoWithHibernate();
         StaffDaoWithHibernate staffDao = new StaffDaoWithHibernate();
-        int insertedApparatusId = 0;
+        int insertedIncidentId = 0;
+
+        // Simulate Staff & Apparatus Id's collected from input form
+        Set<Integer> staffIds = new HashSet<Integer>(Arrays.asList(1002, 1003, 1007));
+        Set<Integer> apparatusIds = new HashSet<Integer>(Arrays.asList(5, 19));
 
         Set<ApparatusEntity> apparatus = new HashSet<ApparatusEntity>(0);
         Set<StaffEntity> staff = new HashSet<StaffEntity>(0);
 
         // http://www.mkyong.com/hibernate/hibernate-many-to-many-relationship-example/
         // Build a set of Apparatus
+        for (int id : apparatusIds) {
+            ApparatusEntity newApparatus = new ApparatusEntity();
+            newApparatus = apparatusDao.getApparatus(id);
+            apparatus.add(newApparatus);
+        }
+
 
         // Build a set of Staff
+        for (int id : staffIds) {
+            StaffEntity newStaff = new StaffEntity();
+            newStaff = staffDao.getStaffById(id);
+            staff.add(newStaff);
+        }
 
+        for (StaffEntity entity : staff) {
+            System.out.println("You added staff: " + entity.toString());
+        }
+
+        // Create Test Incident and set properties
         IncidentEntity testIncident = new IncidentEntity();
+        Date incidentDate = Date.valueOf("9999-12-31");
 
+        testIncident.setDate(incidentDate);
+        testIncident.setAddress("101 N. Main Street");
+        testIncident.setCity("Test City");
+        testIncident.setState("AA");
+        testIncident.setZipcode("99999");
+        testIncident.setIncidentTypeCode(100);
+        testIncident.setPropertyUseCode(419);
+        testIncident.setNarritative("This is a test incident");
+        testIncident.setApparatus(apparatus);
+        testIncident.setStaff(staff);
 
+        insertedIncidentId = incidentDao.addIncident(testIncident);
 
-
+        System.out.println("You added an incident with an id of: " + insertedIncidentId);
     }
 }
